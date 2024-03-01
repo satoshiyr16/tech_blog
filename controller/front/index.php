dae82d69f04c78aa40e5840fa39586d92105a06d
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-03-01 23:36:11 +0900

    仮でtopページに投稿データの追加

--- controller/front/index.php
+++ controller/front/index.php
@@ -5,13 +5,20 @@ namespace controller\front;
 require_once '/Applications/MAMP/htdocs/tech_blog/controller/app/autoload.class.php';
 
 use controller\app\autoload;
+use model\dbCommand;
+use model\entities\post;
 
-$loader = new \Twig_Loader_Filesystem(autoload::TWIG_DIR);
+$db = new dbCommand(autoload::DB_HOST, autoload::DB_USER, autoload::DB_PASS, autoload::DB_NAME);
+$post = new post($db);
+
+$loader = new \Twig_Loader_Filesystem(autoload::TWIG_FRONT_DIR);
 $twig = new \Twig_Environment($loader, [
   'cache' => autoload::CACHE_DIR
 ]);
 
+$title = $db->select('post', 'title', '', ['post_id = ?'], [1]);
+
 $twig->addGlobal('JS_FRONT_DIR', autoload::JS_FRONT_DIR);
 $twig->addGlobal('CSS_FRONT_DIR', autoload::CSS_FRONT_DIR);
 $template = $twig->load('index.html.twig');
-echo $template->render([]);
+echo $template->render(['title' => $title]);
