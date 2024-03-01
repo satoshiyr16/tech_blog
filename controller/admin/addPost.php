35a955d0fe0ef90836939a1219f8ec8da40c4268
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-03-01 23:37:31 +0900

    投稿画面の作成

---
+++ controller/admin/addPost.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace controller\admin;
+
+require_once '/Applications/MAMP/htdocs/tech_blog/controller/app/autoload.class.php';
+
+use controller\app\autoload;
+use model\dbCommand;
+use model\entities\post;
+
+$db = new dbCommand(autoload::DB_HOST, autoload::DB_USER, autoload::DB_PASS, autoload::DB_NAME);
+$post = new post($db);
+
+$loader = new \Twig_Loader_Filesystem(autoload::TWIG_ADMIN_DIR);
+$twig = new \Twig_Environment($loader, [
+  'cache' => autoload::CACHE_DIR
+]);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $title = 'あ';
+  $content = $_POST['postContent'] ?? '';
+  $res = $post->insPostData($title, $content);
+}
+
+
+$twig->addGlobal('CONTROLLER_ADMIN_DIR', autoload::CONTROLLER_ADMIN_DIR);
+$twig->addGlobal('CSS_ADMIN_DIR', autoload::CSS_ADMIN_DIR);
+$template = $twig->load('addPost.html.twig');
+echo $template->render([]);
