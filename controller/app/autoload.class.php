9a12b2aba02770806c580e2413c3ca2a6ef0d5fb
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-03-01 23:35:17 +0900

    autoloadとdb設定ファイルを追加

--- controller/app/autoload.class.php
+++ controller/app/autoload.class.php
@@ -8,19 +8,28 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 class autoload
 {
+  const DB_HOST = 'localhost';
+  const DB_USER = 'root';
+  const DB_PASS = 'root';
+  const DB_NAME = 'tech_blog';
 
+  const APP_URL = 'http://localhost:8888/tech_blog/';
   const APP_DIR = '/Applications/MAMP/htdocs/tech_blog/';
-  const CONTROLLER_DIR = self::APP_DIR . 'controller/app';
-  const TWIG_DIR = self::APP_DIR . 'view/twig/front/';
+  const CONTROLLER_APP_DIR = self::APP_DIR . 'controller/app';
+  const CONTROLLER_ADMIN_DIR = self::APP_URL . 'controller/admin';
+  const CONTROLLER_FRONT_DIR = self::APP_URL . 'controller/front';
+  const TWIG_FRONT_DIR = self::APP_DIR . 'view/twig/front/';
+  const TWIG_ADMIN_DIR = self::APP_DIR . 'view/twig/admin/';
   const JS_FRONT_DIR = self::APP_URL . 'view/js/front';
   const CSS_FRONT_DIR = self::APP_URL . 'view/css/front';
+  const JS_ADMIN_DIR = self::APP_URL . 'view/js/admin';
+  const CSS_ADMIN_DIR = self::APP_URL . 'view/css/admin';
   const CACHE_DIR = false;
-  const APP_URL = 'http://localhost:8888/tech_Blog/';
   // const ENTRY_URL = self::APP_URL . 'shopping/';
 
   public static function loadClass($class)
   {
-    $path = str_replace('\\', '/', self::CONTROLLER_DIR . $class . '.class.php');
+    $path = str_replace('\\', '/', self::APP_DIR . $class . '.class.php');
     require_once $path;
   }
 }
---
+++ model/dbCommand.class.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace model;
+
+class dbCommand
+{
+  public $db_con = null;
+  public $db_host = '';
+  public $db_user = '';
+  public $db_pass = '';
+  public $db_name = '';
+  private $order = '';
+  private $limit = '';
+  private $offset = '';
+  private $groupby = '';
+
+  public function __construct($db_host, $db_user, $db_pass, $db_name)
+  {
+    $this->db_con = $this->connectDB($db_host, $db_user, $db_pass, $db_name);
+    $this->db_host = $db_host;
+    $this->db_user = $db_user;
+    $this->db_pass = $db_pass;
+    $this->db_name = $db_name;
+    $this->order = '';
+    $this->limit = '';
+    $this->offset = '';
+    $this->groupby = '';
+  }
+
+  private function connectDB($db_host, $db_user, $db_pass, $db_name)
+  {
+    $tmp_con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+    if ($tmp_con !== false) {
+      error_log("Database connection established to $db_name.", 0);
+      return $tmp_con;
+    } else {
+      $error_msg = "Connect failed: " . mysqli_connect_error();
+      error_log($error_msg, 0);
+      exit($error_msg);
+    }
+  }
+
+  public function insert($table, $insData = [])
+  {
+    $insDataKey = array_keys($insData);
+    $insDataVal = array_values($insData);
+    $placeHolders = array_fill(0, count($insData), '?');
+
+    // 文字列にする "title, content, tag_id"
+    $columns = implode(", ", $insDataKey);
+    // "?, ?, ?, "
+    $preSt = implode(", ", $placeHolders);
+
+    $sql = "INSERT INTO $table ($columns) VALUES ($preSt)";
+
+    // 自動的にエスケープ
+    $stmt = $this->db_con->prepare($sql);
+    if (!$stmt) {
+      error_log("Prepare failed: " . $this->db_con->error);
+      return false;
+    }
+
+    // バインドする値のタイプを準備 ('s' = string, 'i' = integer, 'd' = double, 'b' = blob)
+    $types = str_repeat('s', count($insDataVal)); // この例では全ての値を文字列として扱います
+
+    // bind_param() に渡すためのパラメータ配列を準備
+    $params = array_merge(array($types), $insDataVal);
+    foreach ($params as $key => $value) {
+      $params[$key] = &$params[$key]; // 参照渡し
+    }
+
+    // パラメータをバインドし、SQLを実行
+    call_user_func_array(array($stmt, 'bind_param'), $params);
+
+    $res = $stmt->execute();
+    if (!$res) {
+      error_log("Execute failed: " . $stmt->error);
+    }
+
+    $stmt->close();
+
+    return $res;
+  }
+
+  private function catchError($errArr = [])
+  {
+    // errorMessageが2番目に格納されている
+    $errMsg = (!empty($errArr[2])) ? $errArr[2] : "";
+    // exitと同じ
+    die("SQLエラーが発生しました。" . $errArr[2]);
+  }
+
+
+  public function select($table, $column = '', $where = '', $arrVal = [])
+  {
+      // SQL文を生成します。
+      $sql = $this->getSql('select', $table, $where, $column);
+
+      // mysqli prepare statement
+      $stmt = $this->db_con->prepare($sql);
+
+      // パラメータがある場合はバインドします。
+      if (!empty($arrVal)) {
+        $types = ''; // パラメータの型を格納する文字列を初期化
+        foreach ($arrVal as $param) {
+            // パラメータの型に応じて型指定子を追加
+            if (is_float($param)) {
+                $types .= 'd'; // double
+            } elseif (is_int($param)) {
+                $types .= 'i'; // integer
+            } else {
+                $types .= 's'; // string
+            }
+        }
+
+        // bind_paramの引数を準備（型指定子とパラメータの値）
+        $bindParams = [];
+        $bindParams[] = &$types; // 参照渡しで型指定子を最初に追加
+        foreach ($arrVal as $key => &$param) {
+            $bindParams[] = &$param; // 各パラメータの参照を追加
+        }
+
+        // bind_paramを呼び出し
+        call_user_func_array([$stmt, 'bind_param'], $bindParams);
+    }
+
+      // SQLを実行
+      $stmt->execute();
+
+      // 結果を取得
+      $result = $stmt->get_result();
+
+      $data = [];
+      while ($row = $result->fetch_assoc()) {
+          $data[] = $row;
+      }
+
+      // ステートメントを閉じる
+      $stmt->close();
+
+      return $data;
+  }
+
+
+  private function getSql($type, $table, $where = '', $column = '')
+  {
+    switch($type) {
+      case 'select':
+        $columnKey = ($column !== '') ? $column : "*";
+      break;
+
+      case 'count':
+        $columnKey = 'COUNT(*) AS NUM';
+      break;
+
+      default:
+      break;
+    }
+
+    $whereSQL = ($where !== '') ? ' WHERE ' . $where : '';
+    $other = $this->groupby . " " . $this->order . " " . $this->limit . " " . $this->offset;
+    $sql = " SELECT " . $columnKey . " FROM " . $table . $whereSQL . $other;
+
+    return $sql;
+  }
+}
