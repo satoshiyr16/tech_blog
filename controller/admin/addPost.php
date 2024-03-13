2a45ffb21d4b393f8dde6f450df1a2489348267f
satoshiyr16 <satoshi.trip.1@gmail.com>
2024-03-13 23:35:00 +0900

    🎨投稿画面のデザイン変更

--- controller/admin/addPost.php
+++ controller/admin/addPost.php
@@ -22,39 +22,10 @@ $twig = new \Twig_Environment($loader, [
 //   $res = $post->insPostData($title, $content);
 // }
 
+$twig->addGlobal('APP_URL', autoload::APP_URL);
 $twig->addGlobal('CONTROLLER_ADMIN_DIR', autoload::CONTROLLER_ADMIN_DIR);
 $twig->addGlobal('CSS_ADMIN_DIR', autoload::CSS_ADMIN_DIR);
 $twig->addGlobal('JS_ADMIN_DIR', autoload::JS_ADMIN_DIR);
 $twig->addGlobal('CSS_MODULE_DIR', autoload::CSS_MODULE_DIR);
 $template = $twig->load('addPost.twig');
 echo $template->render([]);
-
-
-$users = [
-  ['id' => 1, 'pref' => '北海道', 'name' => 'satou'],
-  ['id' => 2, 'pref' => '東京', 'name' => 'tarou'],
-  ['id' => 3, 'pref' => '神奈川', 'name' => 'kobayashi'],
-  ['id' => 4, 'pref' => '東京', 'name' => 'saitou'],
-  ['id' => 5, 'pref' => '神奈川', 'name' => 'yamada'],
-  ['id' => 6, 'pref' => '千葉', 'name' => 'suzuki'],
-  ['id' => 7, 'pref' => '東京', 'name' => 'morita'],
-  ['id' => 8, 'pref' => '埼玉', 'name' => 'takemoto'],
-  ['id' => 9, 'pref' => '北海道', 'name' => 'matsumoto'],
-];
-
-function prefUser(array $users, callable $count) {
-  foreach ($users as $user) {
-    $prefUser[$user['pref']][] = $user['name'];
-  }
-  $count($prefUser);
-}
-
-function prefUserCount(array $sum) {
-  foreach($sum as $val) {
-    if(count($val) > 2) {
-      foreach($val as $value){
-        echo $value;
-      }
-    }
-  }
-}
